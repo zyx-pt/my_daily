a1937f5dd1c14907048183cb89b51ac005f06272
zhengyongxian <1367245342@qq.com>
2022-09-21 09:22:00 +0800

    添加springboot demo

---
+++ springboot-demo/src/test/java/com/zyx/demo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
