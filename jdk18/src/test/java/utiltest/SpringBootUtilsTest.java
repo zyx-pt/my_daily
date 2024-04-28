bec21bd0a017c32cec9af79085126dc7239104fd
yongxian zheng <zhengyongxian@ylzinfo.com>
2024-04-28 11:38:10 +0800

    StopWatch计时工具调整

--- jdk18/src/test/java/utiltest/SpringBootUtilsTest.java
+++ jdk18/src/test/java/utiltest/SpringBootUtilsTest.java
@@ -6,10 +6,31 @@ import entity.Student;
 import org.junit.Test;
 import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StopWatch;
 
 import java.util.List;
 
 public class SpringBootUtilsTest {
+    // StopWatch计时工具
+    @Test
+    public void  testStopWatch() throws InterruptedException {
+        StopWatch stopWatch = new StopWatch("<------------任务耗时统计------------>");
+        stopWatch.start("Task1");
+        System.out.println("当前任务名称：" + stopWatch.currentTaskName());
+        Thread.sleep(1000);
+        stopWatch.stop();
+        stopWatch.start("Task2");
+        System.out.println("当前任务名称：" + stopWatch.currentTaskName());
+        Thread.sleep(3000);
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+
+        System.out.println(stopWatch.shortSummary());
+        System.out.println("所有任务总耗时：" + stopWatch.getTotalTimeMillis());
+        System.out.println("任务总数：" + stopWatch.getTaskCount());
+
+    }
+
     // Assert 断言工具类
     @Test
     public void testAssert(){
